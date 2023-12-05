33c1f1aa1036ca44396abb029b119d024d3bde5f
acascoc098 <acascoc098@g.educaand.es>
2023-12-05 11:50:36 +0100

    Añadimos código para caragra y guardar json.

--- src/main/java/com/iesvdc/acceso/app/Ingesta.java
+++ src/main/java/com/iesvdc/acceso/app/Ingesta.java
@@ -1,8 +1,10 @@
 package com.iesvdc.acceso.app;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,7 +16,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.iesvdc.acceso.app.Conexion.FConex;
+import com.iesvdc.acceso.app.entidades.Cliente;
 import com.iesvdc.acceso.app.entidades.Reserva;
+import com.iesvdc.acceso.app.servicios.ClienteDAO;
+import com.iesvdc.acceso.app.servicios.ReservaDAO;
 
 /**
  * Hello world!
@@ -49,51 +54,25 @@ public class Ingesta
                 create();
         // leemos
         try {
-            List<Usuario> usuarios = new ArrayList<Usuario>();
-            Reader readerUsuarios = new FileReader("data/usuarios.json");
+            List<Cliente> usuarios = new ArrayList<Cliente>();
+            Reader readerUsuarios = new FileReader("bach.json");
             usuarios = gson.fromJson(
-                    readerUsuarios, new TypeToken<List<Usuario>>() {}.getType());
-            System.out.println("Usuarios JSON cargados correctamente");
-            UsuarioDao uDao = new UsuarioDaoImpl();
-            for (Usuario usuario : usuarios) {
+                    readerUsuarios, new TypeToken<List<Cliente>>() {}.getType());
+            System.out.println("Clientes JSON cargados correctamente");
+            ClienteDAO uDao = new ClienteDAOIMP();
+            for (Cliente usuario : usuarios) {
                 System.out.println("insertando: "+usuario);
                 uDao.create(usuario);
             }
             readerUsuarios.close();
-            System.out.println("Usuarios almacenado en la BBDD correctamente.\n");
-
-            List<Instalacion> instalaciones = new ArrayList<Instalacion>();
-            Reader readerInstalaciones = new FileReader("data/instalaciones.json");
-            instalaciones = gson.fromJson(
-                readerInstalaciones, new TypeToken<List<Instalacion>>() {}.getType());
-            System.out.println("Instalaciones JSON cargados correctamente");
-            InstalacionDao iDao = new InstalacionDaoImpl();
-            for (Instalacion instalacion : instalaciones) {
-                System.out.println("insertando: "+instalacion);
-                iDao.create(instalacion);
-            }
-            readerInstalaciones.close();
-            System.out.println("Instalaciones almacenadas en la BBDD correctamente.\n");
-
-            List<Horario> horarios = new ArrayList<Horario>();
-            Reader readerHorarios = new FileReader("data/horarios.json");
-            horarios = gson.fromJson(
-                readerHorarios, new TypeToken<List<Horario>>() {}.getType());
-            System.out.println("Horarios JSON cargados correctamente");
-            HorarioDao hDao = new HorarioDaoImpl();
-            for (Horario horario : horarios) {
-                System.out.println("insertando: "+horario);
-                hDao.create(horario);
-            }
-            readerHorarios.close();
-            System.out.println("Horarios almacenadas en la BBDD correctamente.\n");
+            System.out.println("Cliente almacenado en la BBDD correctamente.\n");
 
             List<Reserva> reservas = new ArrayList<Reserva>();
             Reader readerReservas = new FileReader("data/reservas.json");
             reservas = gson.fromJson(
                 readerReservas, new TypeToken<List<Reserva>>() {}.getType());
             System.out.println("Reservas JSON cargado correctamente");
-            ReservaDao rDao = new ReservaDaoIml();
+            ReservaDAO rDao = new ReservaDaoIml();
             for (Reserva reserva : reservas) {
                 System.out.println("insertando: "+reserva);
                 rDao.create(reserva);
@@ -105,6 +84,23 @@ public class Ingesta
             System.out.println("Archivo no encontrado.");
         } catch (IOException ioe){
             System.out.println("Error de entrada salida.");
+        }
+
+        /**
+         * GUARDAR EL JSON
+         */
+
+         try (PrintWriter prClientes = new PrintWriter(new File("bach.json"))){
+            // Para que salga "bonito"
+            Gson gson = new GsonBuilder().
+                registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter()).
+                setPrettyPrinting().
+                create();
+            // Gson gson = new Gson();
+            prClientes.write(gson.toJson());
+            
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
